8f534feaf47f7a377865a42826958044c4ac47ad
Alain Martel <alain74martel@gmail.com>
2018-02-21 19:19:56 +0000

    Update ARTC.h

--- ARTC.h
+++ ARTC.h
@@ -1,25 +1,23 @@
 #ifndef _ARTC_h
-#define _ARTC_h
+   #define _ARTC_h
 
-#include <Wire.h>
-#include <Time.h>
-#include <DS1307RTC.h>
-#include "Arduino.h"
+   #include <Wire.h>
+   #include <Time.h>
+   #include <DS1307RTC.h>
+   #include "Arduino.h"
 
- class ARTC{
-  tmElements_t tm; 
-  char _alarm[6]="ALARM"; // 00:00
- 
-  int _ATriggered = 0; // To avoid multiple calls during the alarm (one minute) 
+   class ARTC{
+      tmElements_t tm; 
+      char _alarm[6]="ALARM"; // 00:00
+      int _ATriggered = 0; // To avoid multiple calls during the alarm (one minute) 
   
-  public:
-  void setHourTimeToMessage(char *message);
-  int checkAlarm();
+      public:
+         void setHourTimeToMessage(char *message);
+         int checkAlarm();
   
-  void setAlarm(char *alarm);
-  void getAlarm(char *alarm);
-
- };
+         void setAlarm(char *alarm);
+         void getAlarm(char *alarm);
+   };
 
 #endif
 
